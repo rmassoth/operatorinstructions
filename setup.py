a87420caa260dc9be8eca9f2f7c9e617135cb260
Ryan Massoth <rmassoth@baeind.com>
2017-05-10 12:19:26 -0400

    Forgot to change the version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='operatorinstructions',
-      version='1.1.2',
+      version='1.1.3',
       description='BAE Operator Instruction Management',
       author='Ryan Massoth',
       author_email='rmassoth@baeind.com',
